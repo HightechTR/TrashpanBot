d3bee130b198f961f9e75a53ebdc9b3322a0beb2
HightechTR <explosion64.hightechtr@gmail.com>
2025-01-30 05:16:22 +0800

    Add Echo Command

--- src/main/java/TrashpanMain.java
+++ src/main/java/TrashpanMain.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class TrashpanMain {
     public static void main(String[] args) {
         String logo = "⠀⠀⠀⢠⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⠀⠀\n"
@@ -9,9 +11,21 @@ public class TrashpanMain {
                 + "⠀⠙⢧⣿⣿⣿⠀⠀⠈⠻⢿⣿⣿⣿⡿⠋⠀⠀⢀⣘⣧\n"
                 + "⠀⠀⠈⠻⣯⣅⠀⠀⣐⣤⠀⠉⠉⠉⠀⢀⣠⡴⠟⠋⠁\n"
                 + "⠀⠀⠀⠀⠈⢿⣭⣉⣁⣤⠴⠾⠶⠒⠛⠉⠁⠀⠀⠀⠀\n";
+        Scanner in = new Scanner(System.in);
         System.out.println(logo);
-        System.out.println("Raah! I'm TrashpanBot!\nWhat can I do for you today?");
+        System.out.println("Raah! I'm TrashpanBot!");
+        System.out.println("What can I do for you today?");
         System.out.println("__________________________________________________");
+        String command = "";
+        while (!command.equals("bye")) {
+            command = in.nextLine();
+            System.out.println("__________________________________________________");
+            if (!command.equals("bye")) {
+                System.out.println(command);
+                System.out.println("__________________________________________________");
+            }
+        }
         System.out.println("Otsuraki! See you soon!");
+        System.out.println("__________________________________________________");
     }
 }
