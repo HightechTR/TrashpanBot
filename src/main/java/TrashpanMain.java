1e42c35ad120e96aa3f2decaa752d423630f582a
HightechTR <explosion64.hightechtr@gmail.com>
2025-01-31 16:37:14 +0800

    Modify code to follow OOP standard

--- src/main/java/TrashpanMain.java
+++ src/main/java/TrashpanMain.java
@@ -1,7 +1,37 @@
 import java.util.Scanner;
 
 public class TrashpanMain {
-    
+
+    static int listCounter = 0;
+    static Task[] tasks = new Task[100];
+
+    static final String TEXT_LOGO = """
+            ⠀⠀⠀⢠⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⠀⠀
+            ⠀⠀⠀⢸⣿⣎⢛⡶⠤⣤⣤⣤⣀⣀⡴⠞⣩⣵⣾⡇⠀
+            ⠀⠀⠀⠘⣞⡟⠁⠀⠘⠉⠀⠀⠀⠙⠁⣾⣿⣿⣹⠀⠀
+            ⠀⠀⠀⣴⢋⣦⡄⢀⠀⠀⢀⣀⣀⠀⠀⠙⢿⣡⠃⠀⠀
+            ⠀⣠⡞⣡⣿⠟⣻⡆⣠⣾⠿⠿⣷⣍⠀⠀⠈⢳⡄⠀⠀
+            ⢾⣯⠘⣿⡿⣿⣿⡿⢿⣿⣀⣿⣿⣿⣿⡦⠀⠀⠹⣆⠀
+            ⠀⠙⢧⣿⣿⣿⠀⠀⠈⠻⢿⣿⣿⣿⡿⠋⠀⠀⢀⣘⣧
+            ⠀⠀⠈⠻⣯⣅⠀⠀⣐⣤⠀⠉⠉⠉⠀⢀⣠⡴⠟⠋⠁
+            ⠀⠀⠀⠀⠈⢿⣭⣉⣁⣤⠴⠾⠶⠒⠛⠉⠁⠀⠀⠀⠀
+            """;
+
+    static final String TEXT_TASK_LIST = """
+            Current Mode: To-do List
+            A functional to-do list, storing up to 100 tasks.
+            Note: This program does not store tasks after exiting.
+            
+            Commands:
+            "list": Displays full list of tasks
+            "mark <number>": Marks the task labelled with the number as done
+            "unmark <number>": Marks the task labelled with the number as not done
+            "bye": Exits the program
+            Any other input: Adds the input to the list""";
+
+    static final String TEXT_LINE =
+            "__________________________________________________";
+
     /**
      * Parses the input string into an integer.
      * Returns null if the string is not an integer.
@@ -17,97 +47,95 @@ public class TrashpanMain {
         }
     }
 
-    public static void main(String[] args) {
-        String logo = """
-                ⠀⠀⠀⢠⠤⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⣄⠀⠀
-                ⠀⠀⠀⢸⣿⣎⢛⡶⠤⣤⣤⣤⣀⣀⡴⠞⣩⣵⣾⡇⠀
-                ⠀⠀⠀⠘⣞⡟⠁⠀⠘⠉⠀⠀⠀⠙⠁⣾⣿⣿⣹⠀⠀
-                ⠀⠀⠀⣴⢋⣦⡄⢀⠀⠀⢀⣀⣀⠀⠀⠙⢿⣡⠃⠀⠀
-                ⠀⣠⡞⣡⣿⠟⣻⡆⣠⣾⠿⠿⣷⣍⠀⠀⠈⢳⡄⠀⠀
-                ⢾⣯⠘⣿⡿⣿⣿⡿⢿⣿⣀⣿⣿⣿⣿⡦⠀⠀⠹⣆⠀
-                ⠀⠙⢧⣿⣿⣿⠀⠀⠈⠻⢿⣿⣿⣿⡿⠋⠀⠀⢀⣘⣧
-                ⠀⠀⠈⠻⣯⣅⠀⠀⣐⣤⠀⠉⠉⠉⠀⢀⣠⡴⠟⠋⠁
-                ⠀⠀⠀⠀⠈⢿⣭⣉⣁⣤⠴⠾⠶⠒⠛⠉⠁⠀⠀⠀⠀
-                """;
-        Scanner in = new Scanner(System.in);
+    public static void parseInput() {
         String input;
-        String command = "";
-        Task[] tasks = new Task[100];
-        int listCounter = 0;
+        String command;
+        Scanner in = new Scanner(System.in);
 
-        System.out.println(logo);
-        System.out.println("Raah! I'm TrashpanBot!");
-        System.out.println("What can I do for you today?");
-        System.out.println("__________________________________________________");
-
-        System.out.println("""
-                Current Mode: To-do List
-                A functional to-do list, storing up to 100 tasks.
-                Note: This program does not store tasks after exiting.
-                
-                Commands:
-                "list": Displays full list of tasks
-                "mark <number>": Marks the task labelled with the number as done
-                "unmark <number>": Marks the task labelled with the number as not done
-                "bye": Exits the program
-                Any other input: Adds the input to the list""");
-
-        while (!command.equals("bye")) {
-            System.out.println("__________________________________________________");
+        while (true) {
+            System.out.println(TEXT_LINE);
             input = in.nextLine();
             command = input.contains(" ") ? input.substring(0, input.indexOf(" ")) : input;
-            System.out.println("__________________________________________________");
+            System.out.println(TEXT_LINE);
 
             switch (command) {
             case "list":
-                System.out.println("Here's your list:");
-                for (int i = 1; i <= listCounter; i++) {
-                    System.out.print(i + ".[" + tasks[i - 1].getStatusIcon() + "] ");
-                    System.out.println(tasks[i - 1].getDescription());
-                }
+                displayList();
                 break;
 
             case "mark":
-                Integer index1 = tryParse(input.substring(input.indexOf(" ") + 1));
-                if (index1 == null) {
-                    System.out.println("Oops! That's not a number.");
-                } else if (index1 > listCounter) {
-                    System.out.println("Oops! That's not in the list.");
-                } else {
-                    tasks[--index1].setDone(true);
-                    System.out.println("Yay! I've marked this task as done:");
-                    System.out.println("[X] " + tasks[index1].getDescription());
-                }
+                markDone(input);
                 break;
 
             case "unmark":
-                Integer index2 = tryParse(input.substring(input.indexOf(" ") + 1));
-                if (index2 == null) {
-                    System.out.println("Oops! That's not a number.");
-                } else if (index2 > listCounter) {
-                    System.out.println("Oops! That's not in the list.");
-                } else {
-                    tasks[--index2].setDone(false);
-                    System.out.println("Ganbaraki! I've unmarked this task as not done:");
-                    System.out.println("[ ] " + tasks[index2].getDescription());
-                }
+                markNotDone(input);
                 break;
 
             case "bye":
-                break;
+                return;
 
             default:
-                if (listCounter > 99) {
-                    System.out.println("Sorry! The list is full!");
-                } else {
-                    tasks[listCounter] = new Task(command);
-                    listCounter++;
-                    System.out.println("Okay! Added \"" + command + "\" to the list.");
-                }
+                addTask(input);
             }
         }
+    }
+
+    public static void displayList() {
+        System.out.println("Here's your list:");
+        for (int i = 1; i <= listCounter; i++) {
+            System.out.print(i + ".[" + tasks[i - 1].getStatusIcon() + "] ");
+            System.out.println(tasks[i - 1].getDescription());
+        }
+    }
+
+    public static void addTask(String input) {
+        if (listCounter > 99) {
+            System.out.println("Sorry! The list is full!");
+        } else {
+            tasks[listCounter] = new Task(input);
+            listCounter++;
+            System.out.println("Okay! Added \"" + input + "\" to the list.");
+        }
+    }
+
+    public static void markDone(String input) {
+        Integer index = tryParse(input.substring(input.indexOf(" ") + 1));
+        if (index == null) {
+            System.out.println("Oops! That's not a number.");
+        } else if (index > listCounter) {
+            System.out.println("Oops! That's not in the list.");
+        } else {
+            tasks[--index].setDone(true);
+            System.out.println("Yay! I've marked this task as done:");
+            System.out.println("[X] " + tasks[index].getDescription());
+        }
+    }
+
+    public static void markNotDone(String input) {
+        Integer index = tryParse(input.substring(input.indexOf(" ") + 1));
+        if (index == null) {
+            System.out.println("Oops! That's not a number.");
+        } else if (index > listCounter) {
+            System.out.println("Oops! That's not in the list.");
+        } else {
+            tasks[--index].setDone(false);
+            System.out.println("Ganbaraki! I've unmarked this task as not done:");
+            System.out.println("[ ] " + tasks[index].getDescription());
+        }
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(TEXT_LOGO);
+        System.out.println("Raah! I'm TrashpanBot!");
+        System.out.println("What can I do for you today?");
+        System.out.println(TEXT_LINE);
+
+        System.out.println(TEXT_TASK_LIST);
+
+        parseInput();
 
         System.out.println("Otsuraki! See you soon!");
-        System.out.println("__________________________________________________");
+        System.out.println(TEXT_LINE);
     }
 }
